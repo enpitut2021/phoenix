13ad2826eac9b909a420d56a1bf15d5ede737249
高橋晴矢 <>
2021-11-02 17:36:44 +0900

    投稿画面の作成　入力

--- lib/pages/upload_recipe/upload_page.dart
+++ lib/pages/upload_recipe/upload_page.dart
@@ -4,8 +4,7 @@
 import 'package:flutter/material.dart';
 import 'package:phoenix/pages/upload_recipe/make_widget.dart';
 import 'package:phoenix/recipe/recipe_models.dart';
-import 'package:tuple/tuple.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 ///mylibrary~
@@ -17,17 +16,21 @@ class UpLoadRecipe extends StatefulWidget {
   _UpLoadRecipeState createState() => _UpLoadRecipeState();
 }
 
-class _UpLoadRecipeState extends State<UpLoadRecipe> {
-  Recipe recipe = Recipe(
-      id: '',
-      recipename: '',
-      imageurl: '',
-      ingredients: [],
-      cookmethod: [],
-      cookwares: [],
-      explain: [],
-      spices: []
-  );
+class _UpLoadRecipeState extends State<UpLoadRecipe> with MakeWidget {
+  @override
+  void initState() {
+    super.initState();
+    state = setState;
+    recipe = Recipe(
+        id: '',
+        recipename: '',
+        imageurl: '',
+        ingredients: [],
+        cookmethod: [],
+        cookwares: [],
+        explain: [],
+        spices: []);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,38 +38,47 @@ class _UpLoadRecipeState extends State<UpLoadRecipe> {
 
     //ドキュメントID生成関数
     // String makeDocId(){
-    //  return  
+    //  return
     // }
 
+    // var makewidget = MakeWidget(
+    //     recipe: recipe,
+    //     state: () {
+    //       setState(() {});
+    //     });
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('レシピ作成画面'),
-        ),
-        body: setRecipe(screenSize: screenSize, recipe: recipe, onTap: () async {
-          List<Map<String, String>> tmp1 = [], tmp2 = [];
-          for(var _ingredients in recipe.ingredients) {
-            tmp1.add({'name' : _ingredients.name});
-            tmp1.add({'amount': _ingredients.amount});
-          }
-          for(var _spices in recipe.spices) {
-            tmp2.add({'name' : _spices.name});
-            tmp2.add({'amount': _spices.amount});
-          }
-          await FirebaseFirestore.instance
-              .collection('recipes')
-              .doc(/*makeDocId*/)
-              .set({
-            'id': recipe.id,
-            'recipe_name': recipe.recipename,
-            'imageurl': recipe.imageurl,
-            'ingredients': tmp1,
-            'cookmethod': recipe.cookmethod,
-            'cookwares': recipe.cookwares,
-            'explain': recipe.explain,
-            'spices': tmp2,
-            // 'minutes': recipe.minutes
-          });
-        }),
+      appBar: AppBar(
+        title: const Text('レシピ作成画面'),
+      ),
+      body: setRecipe(
+          context: context,
+          screenSize: screenSize,
+          onTap: () async {
+            List<Map<String, String>> tmp1 = [], tmp2 = [];
+            for (var _ingredients in recipe.ingredients) {
+              tmp1.add({'name': _ingredients.name});
+              tmp1.add({'amount': _ingredients.amount});
+            }
+            for (var _spices in recipe.spices) {
+              tmp2.add({'name': _spices.name});
+              tmp2.add({'amount': _spices.amount});
+            }
+            await FirebaseFirestore.instance
+                .collection('recipes')
+                .doc(/*makeDocId*/)
+                .set({
+              'id': recipe.id,
+              'recipe_name': recipe.recipename,
+              'imageurl': recipe.imageurl,
+              'ingredients': tmp1,
+              'cookmethod': recipe.cookmethod,
+              'cookwares': recipe.cookwares,
+              'explain': recipe.explain,
+              'spices': tmp2,
+              // 'minutes': recipe.minutes
+            });
+          }),
     );
   }
 }
