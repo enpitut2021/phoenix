5606378b6f0790df38c8625da1eebd852c3ac57a
yu-ta38 <hyuta@outlook.jp>
2021-11-10 11:22:14 +0900

    本日の進捗：ユーザーid作成の関数を作成しました

--- lib/Model/user/user_model.dart
+++ lib/Model/user/user_model.dart
@@ -1,15 +1,60 @@
+import 'dart:convert';
+import 'dart:async';
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
 class UserModel {
   String name = "";
   int id = 0;
   int makenum = 0;
-  List<int> friendid = [];
-  List<int> recipeid = [];
-  List<int> recentrecipeid = [];
+  List<int> friends = [];
+  // List<int> recipeid = [];
+  List<int> recentrecipe = [];
 
-  UserModel(
-      {required name,
-      required id,
-      required makenum,
-      required friendid,
-      required recipeid});
+  Future<void> loadUserId() async{
+    final String _loadData =
+    await rootBundle.loadString('lib/Model/user/user_info.json');
+    final jsonResponse = json.decode(_loadData);
+
+    id = jsonResponse['id'];
+  }
+
+  void userFetch() async {
+    String id = this.id.toString();
+    await FirebaseFirestore.instance
+                .collection('User')
+                .doc(id)
+                .get().then(
+                  (value) => {
+                    name = value.get('name'),
+                    // friends = value.get('friends'),
+                    // recentrecipe = value.get('recent_recipe')
+                  }
+                );
+  }
+
+  //  Map<String, dynamic> toJson() => {        
+  //    'id': this.id,
+  //   };
+
+  void createId() async {
+    final _store = FirebaseFirestore.instance;
+    String id = "";
+    await _store.collection('User').orderBy('id', descending: true).limit(1).get().then((value)=>{
+      this.id = value.docs[0].get('id')+1,
+      id = this.id.toString(),
+      print(this.id)
+    });
+    _store.collection('User').doc(id).set({
+      'name': '鬼岡',
+      'id' : this.id
+    });
+    // final _filepath = File('lib/Model/user/user_info.json');
+
+    // List<Map> ids = [];
+
+    // _filepath.writeAsString(json.decode(ids));
+  }
+    
 }
